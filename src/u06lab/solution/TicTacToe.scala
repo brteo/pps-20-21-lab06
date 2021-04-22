0ac0634a996a4aa613b0ca82e1ae578e0051ea05
brteo <51654355+brteo@users.noreply.github.com>
2021-04-22 18:27:24 +0200

    Exercise 3 (step 1/2)

--- src/u06lab/solution/TicTacToe.scala
+++ src/u06lab/solution/TicTacToe.scala
@@ -12,35 +12,44 @@ object TicTacToe {
   type Board = List[Mark]
   type Game = List[Board]
 
-  def find(board: Board, x: Int, y: Int): Option[Player] = ???
+  def find(board: Board, x: Int, y: Int): Option[Player] = board.collectFirst( { case Mark(`x`,`y`, p) => p } )
 
-  def placeAnyMark(board: Board, player: Player): Seq[Board] = ???
+  def placeAnyMark(board: Board, player: Player): Seq[Board] = {
+    for (x <- 0 to 2; y <- 0 to 2; if find(board, x, y).isEmpty ) yield Mark(x, y, player) :: board
+  }
 
   def computeAnyGame(player: Player, moves: Int): Stream[Game] = ???
 
-  def printBoards(game: Seq[Board]): Unit =
+  def printBoards(game: Seq[Board])(p: String => Any): Unit =
     for (y <- 0 to 2; board <- game.reverse; x <- 0 to 2) {
-      print(find(board, x, y) map (_.toString) getOrElse ("."))
-      if (x == 2) { print(" "); if (board == game.head) println()}
+      p(find(board, x, y) map (_.toString) getOrElse ("."))
+      if (x == 2) { p(" "); if (board == game.head) p("\n")}
     }
 
+  //implicit val printToConsole: String => Unit = print
+  def printBoardsToConsole(game: Seq[Board]): Unit = printBoards(game)(print)
+  def printBoardsToString(game: Seq[Board]): String = { var m:String = ""; printBoards(game)(m+=_); m }
+}
+
+import TicTacToe._
+object TryTicTacToe extends App {
   // Exercise 1: implement find such that..
   println(find(List(Mark(0,0,X)),0,0)) // Some(X)
   println(find(List(Mark(0,0,X),Mark(0,1,O),Mark(0,2,X)),0,1)) // Some(O)
   println(find(List(Mark(0,0,X),Mark(0,1,O),Mark(0,2,X)),1,1)) // None
 
   // Exercise 2: implement placeAnyMark such that..
-  printBoards(placeAnyMark(List(),X))
+  printBoardsToConsole(placeAnyMark(List(),X))
   //... ... ..X ... ... .X. ... ... X..
   //... ..X ... ... .X. ... ... X.. ...
   //..X ... ... .X. ... ... X.. ... ...
-  printBoards(placeAnyMark(List(Mark(0,0,O)),X))
+  printBoardsToConsole(placeAnyMark(List(Mark(0,0,O)),X))
   //O.. O.. O.X O.. O.. OX. O.. O..
   //... ..X ... ... .X. ... ... X..
   //..X ... ... .X. ... ... X.. ...
 
   // Exercise 3 (ADVANCED!): implement computeAnyGame such that..
-  computeAnyGame(O, 4) foreach {g => printBoards(g); println()}
+  computeAnyGame(O, 4) foreach {g => printBoardsToConsole(g); println()}
   //... X.. X.. X.. XO.
   //... ... O.. O.. O..
   //... ... ... X.. X..
---
+++ test/u06lab/solution/TicTacToeTest.scala
@@ -0,0 +1,41 @@
+package u06lab.solution
+
+import org.junit.jupiter.api.{Assertions, Test}
+import Assertions._
+import TicTacToe._
+
+class TicTacToeTest {
+
+  @Test
+  def findTest() = {
+    assertEquals(Some(X), find(List(Mark(0,0,X)),0,0))
+    assertEquals(Some(O), find(List(Mark(0,0,X),Mark(0,1,O),Mark(0,2,X)),0,1))
+    assertEquals(None, find(List(Mark(0,0,X),Mark(0,1,O),Mark(0,2,X)),1,1))
+  }
+
+  @Test
+  def placeAnyMarkTest() = {
+    // Exercise 2: implement placeAnyMark such that..
+    val output1:String = "... ... ..X ... ... .X. ... ... X.. \n... ..X ... ... .X. ... ... X.. ... \n..X ... ... .X. ... ... X.. ... ... \n"
+    printBoardsToConsole(placeAnyMark(List(),X))
+    assertEquals(output1,printBoardsToString(placeAnyMark(List(),X)))
+
+    val output2:String = "O.. O.. O.X O.. O.. OX. O.. O.. \n... ..X ... ... .X. ... ... X.. \n..X ... ... .X. ... ... X.. ... \n"
+    printBoardsToConsole(placeAnyMark(List(Mark(0,0,O)),X))
+    assertEquals(output2,printBoardsToString(placeAnyMark(List(Mark(0,0,O)),X)))
+  }
+
+  @Test
+  def computeAnyGameTest() = {
+    // Exercise 3 (ADVANCED!): implement computeAnyGame such that..
+    //computeAnyGame(O, 4) foreach {g => printBoards(g); println()}
+    //... X.. X.. X.. XO.
+    //... ... O.. O.. O..
+    //... ... ... X.. X..
+    //              ... computes many such games (they should be 9*8*7*6 ~ 3000).. also, e.g.:
+    //
+    //... ... .O. XO. XOO
+    //... ... ... ... ...
+    //... .X. .X. .X. .X.
+  }
+}
